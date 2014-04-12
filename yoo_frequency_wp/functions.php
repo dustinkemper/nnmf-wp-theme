5eedd3e2e3f01a389a6ac60e9c68d6a3dc1ed8f3
Dustin Kemper <dustin@tenfoldviusal.com>
2014-04-12 17:13:59 -0400

    featured posts
    added new featured posts widget and a default post image for fb

--- yoo_frequency_wp/functions.php
+++ yoo_frequency_wp/functions.php
@@ -12,3 +12,19 @@ if (version_compare(PHP_VERSION, '5.3', '>=')) {
     // bootstrap warp
     require(__DIR__.'/warp.php');
 }
+
+function insert_image_src_rel_in_head() {
+  global $post;
+  if ( !is_singular()) //if it is not a post or a page
+    return;
+  if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+    $default_image="http://northernnights.org/wp-content/themes/yoo_frequency_wp/images/NNMF/nnmf-default-post.jpg"; 
+  }
+  else{
+    $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+    echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+  }
+  echo "
+";
+}
+add_action( 'wp_head', 'insert_image_src_rel_in_head', 5 );
